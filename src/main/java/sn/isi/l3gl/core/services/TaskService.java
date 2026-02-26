4a4b9f45b0e10d36828cab65ad5bbb55c3786bcb
devab007 <tallaabdoulwahab207@gmail.com>
2026-02-26 17:47:26 +0000

    Ajout de la méthode updateStatus (v0.2.0-SNAPSHOT)

--- src/main/java/sn/isi/l3gl/core/services/TaskService.java
+++ src/main/java/sn/isi/l3gl/core/services/TaskService.java
@@ -5,6 +5,7 @@ import sn.isi.l3gl.core.entities.TaskStatus;
 import sn.isi.l3gl.core.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -17,6 +18,17 @@ public class TaskService {
         if (task.getStatus() == null) {
             task.setStatus(TaskStatus.TODO);
         }
+        return taskRepository.save(task);
+    }
+
+    public List<Task> listTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task updateStatus(Long id, TaskStatus status) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tâche non trouvée avec l'id : " + id));
+        task.setStatus(status);
         return taskRepository.save(task);
     }
 }
